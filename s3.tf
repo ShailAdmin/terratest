db727ec1f036915d17cd486a43bef078bfd7548b
Shailesh Gupta <73438626+ShailAdmin@users.noreply.github.com>
2024-06-05 00:40:54 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,26 +1,11 @@
 provider "aws" {
-  access_key = "AKIAUD7KQ6YJZNUBW55I"
-  secret_key = "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"
-}
-terraform {
-  backend "s3" {
-    bucket         = "terraform.poc.backend  "
-    key            = "terraform/terraform.tfstate"
-    region         = "us-east-1" 
-  }
+    region = "us-east-1"  
 }
 
-resource "aws_s3_bucket" "backend1" {
-  bucket = "terraform-poc-bucket-11"
+resource "aws_instance" "foo" {
+  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  instance_type = "t2.micro"
   tags = {
-    Name        = "terraform-state"
-    Env        = "Dev"
+      Name = "TF-Instance"
   }
-  versioning {
-    enabled = true
-  }
-}
-resource "aws_s3_bucket_object" "FolderForStateFile" {
-  bucket = aws_s3_bucket.backend.id
-  key    = "terraform/state/"
 }
