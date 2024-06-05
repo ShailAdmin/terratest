099ff0b2056653cf612abd4fa3a595061aef5dea
Shailesh Gupta <73438626+ShailAdmin@users.noreply.github.com>
2024-06-05 16:01:01 +0530

    Update backend_s3.tf

--- backend_s3.tf
+++ backend_s3.tf
@@ -1,10 +1,1 @@
-resource "aws_s3_bucket" "backend" {
-  bucket = "terraform-poc-bucket-1"
-  tags = {
-    Name        = "terraform-state"
-    Env        = "Dev"
-  }
-  versioning {
-    enabled = true
-  }
-}
+
