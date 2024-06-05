a487a23a99507b84ca384fb608ebe575f8ea5259
Shailesh Gupta <73438626+ShailAdmin@users.noreply.github.com>
2024-06-05 15:58:32 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,14 +1,6 @@
 provider "aws" {
     region = "us-east-1"  
 }
-
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
-  }
-}
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-unique-bucket-name" # Change this to your desired bucket name
   acl    = "private" # Access control list, you can change this as needed
