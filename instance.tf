ba211ed6e730739e24b07cdc87767eaeaac1b0d9
Shailesh Gupta <73438626+ShailAdmin@users.noreply.github.com>
2024-06-05 16:13:02 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -5,3 +5,10 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "jenkinstest1234testjenkins" # Change this to your desired bucket name
   acl    = "private" # Access control list, you can change this as needed
 }
+resource "aws_instance" "my_instance" {
+  ami           = "ami-00beae93a2d981137" # Change this to your desired AMI ID
+  instance_type = "t2.micro"      # Change this to your desired instance type
+  tags = {
+    Name = "MyEC2Instance"        # Change this to your desired name
+  }
+}
