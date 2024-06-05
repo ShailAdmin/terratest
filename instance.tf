92c44325e17e8120144d8bbeabc66c05892edcdf
Shailesh Gupta <73438626+ShailAdmin@users.noreply.github.com>
2024-06-05 16:09:30 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,6 +2,6 @@ provider "aws" {
     region = "ap-south-1"  
 }
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "jenkinstest1234" # Change this to your desired bucket name
+  bucket = "jenkinstest1234testjenkins" # Change this to your desired bucket name
   acl    = "private" # Access control list, you can change this as needed
 }
